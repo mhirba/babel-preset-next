bae59e35848000392b21fd0b24c8451f6d963560
Majdi Toumi <majdi@mhirba.com>
2021-04-11 22:00:29 +0200

    update dev > dependdencies

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '**/*.+(js|ts|json)': ['prettier', 'jest --bail --findRelatedTests'],
-  /**
-   * @note --bail used to exit the test suite immediately upon a number (1) of failing test suite
-   * @note -findRelatedTests to run the tests depending on our jest configuration
-   */
-}
